d78c8f6506b3d016aad2056853baae9c58daf879
zjl <fl_ower4486@outlook.com>
2024-07-20 16:12:51 +0800

    找到排序问题的思路了

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <script setup>
 import { listDirectory, getFileContent } from '@/requests/dav';
-import { extractedImages, extractAndDisplayImages } from '@/requests/extract';
+import { extractedImages, extractAndDisplayImages, extractAndDisplayImages_sync, compareLabels } from '@/requests/extract';
 let num = $ref(0);
 let directoryItems = $ref('');
 let zipFile = $ref(null);
-
+let show_imgs = $ref([]);
 //获取压缩包并解压
-let davpromise = getFileContent('/漫画/做韵律操的妈妈/第0话.zip');
+let davpromise = getFileContent('/漫画/极品家丁宁坠篇/第0话.zip');
 davpromise.then((res) => {
   console.log('压缩包获取', res);
   zipFile = res;
-  extractAndDisplayImages(zipFile);
-  console.log('漫画', extractedImages.value);
+  extractAndDisplayImages_sync(zipFile);
+  show_imgs = extractedImages.value;
+  // console.log('show_imgs', show_imgs.sort(compareLabels));
+  // console.log('show_imgs', show_imgs[13].index);
 });
 
 function click() {
+  show_imgs.sort(compareLabels);
+  console.log('排序后', show_imgs);
   num++;
 }
 </script>
@@ -28,8 +32,8 @@ function click() {
     <!-- <p v-for="item in directoryItems" :key="item.basename" class="scrollbar-demo-item">
       <span>{{ item.basename }}</span>
     </p> -->
-    <div v-for="item in extractedImages" :key="item">
-      <img :src="item" />
+    <div v-for="item in show_imgs" :key="item.index">
+      <img :src="item.img" />
     </div>
   </el-scrollbar>
 </template>
